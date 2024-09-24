ce9c34e7634e5611695f4238580d3df6ffda68a9
thishahid <152827525+thishahid@users.noreply.github.com>
2024-09-25 02:38:03 +0530

    Create JDBCConnectionManager.java

---
+++ src/JDBCConnectionManager.java
@@ -0,0 +1,25 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JDBCConnectionManager {
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            // Register the MySQL JDBC driver
+            Class.forName("com.mysql.cj.jdbc.Driver"); // for MySQL 8.x.x versions
+
+            // Establish the connection
+            connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/student_db", "root", "shahid@6267");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("MySQL JDBC Driver not found.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Failed to make a connection.");
+        }
+        return connection;
+    }
+}
+
